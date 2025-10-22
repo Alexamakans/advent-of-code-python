62afa5d83cabe88d64b867659e00463c038829a3
Alexamakans <fougner.alexander@gmail.com>
2025-10-22 08:08:50 +0200

    Fix day01 part 2

--- aoc/years/y2017/day01/solver.py
+++ aoc/years/y2017/day01/solver.py
@@ -10,11 +10,5 @@ def part_two(input: str) -> str:
     arr = input.strip()
     half = len(arr) // 2
     return str(
-        sum(
-            [
-                int(x)
-                for i, x in enumerate(arr)
-                if i < len(arr) - 1 and x == arr[(i + half) % len(arr)]
-            ]
-        )
+        sum([int(x) for i, x in enumerate(arr) if x == arr[(i + half) % len(arr)]])
     )
